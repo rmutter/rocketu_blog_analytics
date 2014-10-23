34889e77ce27e4c0122b960771813962d1b68d95
Rudy Mutter <rudy@yetihq.com>
2014-10-23 03:57:43 -0700

    appenlight

--- rocketu_blog_analytics/settings.py
+++ rocketu_blog_analytics/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'appenlight_client.django_middleware.AppenlightMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
